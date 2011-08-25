d8b21a31297c8c26ba1626a1339961b453d58d9a
Jani Nieminen <jpknie@gmail.com>
2011-08-25 22:21:17 +0300

    Introduce the layout for metadata activity

---
+++ src/com/jn/android/guvnor/MetadataActivity.java
@@ -0,0 +1,14 @@
+package com.jn.android.guvnor;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MetadataActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+		
+}
