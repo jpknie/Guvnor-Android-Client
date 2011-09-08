e6a3c366c2af52b7f8cc06743f36fee0bc29013e
Jani Nieminen <jpknie@gmail.com>
2011-09-08 17:16:10 +0300

    Remove redundant comments.

--- src/com/jn/android/guvnor/MetadataActivity.java
+++ src/com/jn/android/guvnor/MetadataActivity.java
@@ -29,18 +29,7 @@ public class MetadataActivity extends Activity {
 		String queryId = Long.toString(id);
 		Cursor c = cr.query(DataProvider.CONTENT_URI_METADATA, projection, DataProvider.C_METADATA_FK + "=" + queryId, null, DataProvider.C_METADATA_UUID);
 		c.moveToFirst();
-	/*
-		/uuidSmall" android:layout_height="wrap_content" android:layout_width="wrap_content" android:textAppearance="?android:attr/textAppearanceSmall" android:text="TextView"></TextView>
-        <TextView android:id="@+id/stateLarge" android:layout_height="wrap_content" android:layout_width="wrap_content" android:textAppearance="?android:attr/textAppearanceMedium" android:text="State"></TextView>
-        <TextView android:id="@+id/stateSmall" android:layout_height="wrap_content" android:layout_width="wrap_content" android:textAppearance="?android:attr/textAppearanceSmall" android:text="TextView"></TextView>
-        <TextView android:id="@+id/createdLarge" android:layout_height="wrap_content" android:layout_width="wrap_content" android:textAppearance="?android:attr/textAppearanceMedium" android:text="Created"></TextView>
-        <TextView android:id="@+id/createdSmall" android:layout_height="wrap_content" android:layout_width="wrap_content" android:textAppearance="?android:attr/textAppearanceSmall" android:text="TextView"></TextView>
-        <TextView android:id="@+id/lastContribLarge" android:layout_height="wrap_content" android:layout_width="wrap_content" android:textAppearance="?android:attr/textAppearanceMedium" android:text="Last Contributor"></TextView>
-        <TextView android:id="@+id/lastContribSmall" android:layout_height="wrap_content" android:layout_width="wrap_content" android:textAppearance="?android:attr/textAppearanceSmall" android:text="TextView"></TextView>
-        <TextView android:id="@+id/lastModifiedLarge" android:layout_height="wrap_content" android:layout_width="wrap_content" android:textAppearance="?android:attr/textAppearanceMedium" android:text="Last Modified"></TextView>
-        <TextView android:id="@+id/lastModifiedSmall" android:layout_height="wrap_content"
-		*/
-		
+	
 		TextView uuidView = (TextView)findViewById(R.id.uuidSmall);
 		TextView stateView = (TextView)findViewById(R.id.stateSmall);
 		TextView createdView = (TextView)findViewById(R.id.createdSmall);
