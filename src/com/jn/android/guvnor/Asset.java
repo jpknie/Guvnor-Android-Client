24ca8d26be249b1fc8f3a9453f71c77f750938d0
Jani Nieminen <jpknie@gmail.com>
2011-09-10 10:57:06 +0300

    Create class for Assets

---
+++ src/com/jn/android/guvnor/Asset.java
@@ -0,0 +1,27 @@
+package com.jn.android.guvnor;
+
+import java.util.List;
+
+import android.content.ContentProviderOperation;
+
+public class Asset implements PersistentResource {
+
+	@Override
+	public void addUpdateOperationTo(List<ContentProviderOperation> operationList) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addInsertOperationTo(List<ContentProviderOperation> operationList) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void addDeleteOperationTo(List<ContentProviderOperation> operationList) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
